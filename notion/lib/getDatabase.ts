65fdc27f504a9f21ce3c174fe04864b970afa72f
norafu-dev <norafudev@gmail.com>
2025-03-31 00:11:56 +0900

    fix bug of unstable_cache params

--- notion/lib/getDatabase.ts
+++ notion/lib/getDatabase.ts
@@ -1,11 +1,13 @@
 import notion from "@/notion/lib/client";
 import { unstable_cache } from "next/cache";
 
+const databaseId = process.env.NOTION_DATABASE_ID as string;
+
 // use unstable_cache to cache the database, use cache tag to invalidate the cache
 const getDatabase = unstable_cache(
   async () => {
     const response = await notion.databases.query({
-      database_id: process.env.NOTION_DATABASE_ID as string,
+      database_id: databaseId,
       filter: {
         property: "Status",
         status: {
@@ -22,8 +24,11 @@ const getDatabase = unstable_cache(
 
     return response.results;
   },
-  ["notion-database"],
-  { revalidate: 60 * 60 * 24 }
+  [databaseId],
+  {
+    tags: ["notion-database"],
+    revalidate: 60 * 60 * 24,
+  }
 );
 
 export default getDatabase;
