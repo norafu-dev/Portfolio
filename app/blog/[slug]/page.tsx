7036831a64287bb6019e55169b8b017d601f0722
norafu-dev <norafudev@gmail.com>
2025-04-01 23:33:47 +0900

    ui

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,10 @@
-const page = () => {
-  return <div>page</div>;
+import getDatabase from "@/notion/lib/getDatabase";
+
+const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
+  const database = await getDatabase();
+  const post = database.find((post) => post.slug === slug);
+  return <div>{post?.title}</div>;
 };
 
 export default page;
